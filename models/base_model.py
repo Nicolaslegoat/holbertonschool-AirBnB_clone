c8da99274a12fe78d75f5f25f8992a2a41c27a3e
Nicolaslegoat <n.mancino03@gmail.com>
2023-11-02 03:06:21 -0700

    add file to task 4

--- models/base_model.py
+++ models/base_model.py
@@ -10,10 +10,15 @@ class BaseModel:
     """
     defines all common attributes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializer for BaseModel class
         """
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
