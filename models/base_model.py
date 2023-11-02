ddae6c403c6eb75bf4c06fb462318f67882de7a2
Nicolaslegoat <n.mancino03@gmail.com>
2023-11-02 02:45:31 -0700

    changing comm statement

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,39 @@
 #!/usr/bin/python3
-""" Project AirBnB Clone
+"""
+Project AirBnB Clone
 """
 from datetime import datetime
 import uuid
 
 
 class BaseModel:
-    """ defines all common attributes
+    """
+    defines all common attributes
     """
     def __init__(self):
-        """ Initializer for BaseModel class
+        """
+        Initializer for BaseModel class
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """ Returns a string representaton of a BaseModel
+        """
+        Returns a string representaton of a BaseModel
         """
         return "[{}] ({}) {}"\
             .format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """Updates the public instances attribute
+        """
+        Updates the public instances attribute
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-            returns a dictionary containing all keys/values of __dict__
+        returns a dictionary containing all keys/values of __dict__
         """
         data = self.__dict__.copy()
         data['__class__'] = self.__class__.__name__
