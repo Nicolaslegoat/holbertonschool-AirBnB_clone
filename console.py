45e371d4f54d6c07993f60cadc6e66411c00ac36
Nicolaslegoat <n.mancino03@gmail.com>
2023-11-03 04:08:52 -0700

    create func in co nsole

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 A module that contains the console class.
 '''
 import cmd
+import models
 
 
 class HBNBCommand(cmd.Cmd):
@@ -28,6 +29,26 @@ class HBNBCommand(cmd.Cmd):
         A method for enter prompt to do nothing.
         '''
         pass
+
+    def do_create(self, args):
+        if not args:
+            print("** class name missing **")
+            return
+        
+        if args not in globals():
+            print("** class doesn't exist **")
+            return
+
+        cls = globals()[args]
+        new_instance = cls()
+
+        models.storage.save()
+
+        return new_instance.id
+
+    def do_show(self):
+
+
 
 
 if __name__ == '__main__':
